9171c0edcd0254e0fe70b654aee6be6fd29aa370
WEB Contest <contest@kitm.local>
2024-03-29 12:54:31 +0200

    Save commit

--- Frontend/src/components/Navigation.tsx
+++ Frontend/src/components/Navigation.tsx
@@ -1,4 +1,5 @@
 import { NavCard } from './NavCard';
+import { Icon } from '@iconify/react';
 import { useEffect } from 'react';
 
 const BaseURL = 'http://172.16.50.58:5000';
@@ -17,6 +18,14 @@ export function Navbar() {
     return (
         <section className="w-[360px] h-full bg-slate-700 p-4">
             <main className="w-full h-full">
+                <header className='pb-8 flex items-center justify-between'>
+                    <h1 className='text-slate-200 font-black text-4xl'>KITM DI</h1>
+
+                    <div className='w-8 h-8 rounded-xl bg-slate-800 grid place-items-center'>
+                        <Icon icon="tabler:user" className="w-6 h-6 text-slate-200"/>
+                    </div>
+                </header>
+
                 <NavCard/>
             </main>
         </section>
